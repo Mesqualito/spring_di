9d50e4e82e74e9c8977a7c68be5e080ba9637d43
Jochen <jo@hassfurter.com>
2020-01-14 15:31:57 +0100

    Spring boot framework 2.2.2.RELEASE

---
+++ src/main/java/com/eigenbaumarkt/spring_di_ways/SpringDiWaysApplication.java
@@ -0,0 +1,13 @@
+package com.eigenbaumarkt.spring_di_ways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiWaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDiWaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eigenbaumarkt/spring_di_ways/SpringDiWaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eigenbaumarkt.spring_di_ways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiWaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
